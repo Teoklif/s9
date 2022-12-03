432502336b1f86a24eff1945c6ee8cd6b03c5a74
alpryakh <alpryakh@gmail.com>
2022-12-03 15:44:17 +0300

    ex 68

---
+++ ex 68/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Enter firsrt number:");
+int FirstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second number:");
+int SecondNumber = Convert.ToInt32(Console.ReadLine());
+
+int A(int n, int m)
+{
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return A(n - 1, 1);
+    else
+      return A(n - 1, A(n, m - 1));
+}
+
+Console.WriteLine($"Result = {A(FirstNumber, SecondNumber)}");
