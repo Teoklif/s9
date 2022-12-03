688002ade1a949abaf738f7137496655f38e848b
alpryakh <alpryakh@gmail.com>
2022-12-03 15:43:40 +0300

    ex 66

---
+++ ex 66/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+Console.Write("Enter firsrt number:");
+int FirstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second number:");
+int SecondNumber = Convert.ToInt32(Console.ReadLine());
+int NumbersSum(int a, int b)
+{
+    int result = 0;
+    if ( a < b)
+    {
+    for (int i = a; i<=b; i++)
+    {
+        result += i;
+    }
+    return result;
+    }
+    else
+    {
+        for (int i = b; i<=a; i++)
+    {
+        result += i;
+    }
+    return result;
+    }
+
+}
+
+Console.Write($"Sum = {NumbersSum(FirstNumber, SecondNumber)}");
