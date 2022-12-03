aa83da78adb5fef0b25cd2542b4520ccba013224
alpryakh <alpryakh@gmail.com>
2022-12-03 15:48:55 +0300

    68

--- ex 68/Program.cs
+++ ex 68/Program.cs
@@ -6,13 +6,13 @@ int SecondNumber = Convert.ToInt32(Console.ReadLine());
 
 int A(int n, int m)
 {
-  if (n == 0)
-    return m + 1;
-  else
-    if ((n != 0) && (m == 0))
-      return A(n - 1, 1);
+    if (n == 0)
+        return m + 1;
     else
-      return A(n - 1, A(n, m - 1));
+      if ((n != 0) && (m == 0))
+        return A(n - 1, 1);
+    else
+        return A(n - 1, A(n, m - 1));
 }
 
 Console.WriteLine($"Result = {A(FirstNumber, SecondNumber)}");
