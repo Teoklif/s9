f8d6d1839c0ec10119d29eabc4af7b6092721887
alpryakh <alpryakh@gmail.com>
2022-12-03 15:43:06 +0300

    ex 64

---
+++ ex 64/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Enter your number:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string NumberRec(int a, int b)
+{
+    if (b<=a) return $"{a} " + NumberRec(a-1, b);
+    else return String.Empty;
+}
+
+Console.Write(NumberRec(number, 1));
